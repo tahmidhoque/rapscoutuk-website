804ee0eb7001234330513850f5fc71d03a52b55a
tahmid-hoque-procode <tahmid.hoque@procode.co.uk>
2026-03-23 00:58:02 +0000

    Update social media links in configuration to reflect new usernames.

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,7 +2,7 @@
  * Replace placeholder URLs with your live social profiles.
  */
 export const SOCIAL_LINKS = {
-  youtube: 'https://www.youtube.com/@rapscout',
-  instagram: 'https://www.instagram.com/rapscout',
-  tiktok: 'https://www.tiktok.com/@rapscout',
+  youtube: 'https://www.youtube.com/@rapscouts',
+  instagram: 'https://www.instagram.com/rapscouts',
+  tiktok: 'https://www.tiktok.com/@rapscoutuk',
 } as const
